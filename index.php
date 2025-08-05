5a917f8ee0389ec5e35edcd6c4097f4739725692
xshahbazintel <xshahbazintel@gmail.com>
2025-08-05 11:39:58 +0530

    updated env

--- index.php
+++ index.php
@@ -103,6 +103,26 @@
             </div>
             <div class="row it_works">
               <?php
+              function loadEnv($path)
+                {
+                    if (!file_exists($path)) {
+                        return false;
+                    }
+
+                    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+                    foreach ($lines as $line) {
+                        if (strpos(trim($line), '#') === 0) {
+                            continue;
+                        }
+
+                        list($name, $value) = explode('=', $line, 2);
+                        $name = trim($name);
+                        $value = trim($value);
+                        putenv(sprintf('%s=%s', $name, $value));
+                    }
+                    return true;
+                       loadEnv(__DIR__ . '/.env');
+
 
                         // $link = mysqli_connect('172.20.1.101', 'ecomuser', 'ecompassword', 'ecomdb');
                         // Fetch database connection details directly from environment variables
